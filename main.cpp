ef9e9003ce96084eadd5687ba9c08ae4f179ded3
baishijie <1139179328@qq.com>
2026-02-04 14:36:55 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include "CGAL_test.h"
+
+int main() {
+
+
+
+
+    return 0;
+}
---
+++ src/CGAL/include/CGAL_test.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+#include <Eigen/Dense>
+
+
+
+using Vector2d = Eigen::Vector2d;
+using Vector3d = Eigen::Vector3d;
+using VectorXi = Eigen::VectorXi;
