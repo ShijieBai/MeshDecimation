ef5dde5f6693a76f68724fefd08b9e02de31d668
baishijie <1139179328@qq.com>
2026-02-10 11:29:24 +0800

    save

--- src/pmp/include/pmp_test.h
+++ src/pmp/include/pmp_test.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <pmp/io/io_flags.h>
+#include <pmp/io/io.h>
+#include <pmp/algorithms/decimation.h>
+
+namespace pmp_test {
+
+	void pmp_test(const std::string &input_file, const std::string &out_file, const double &angle);
+
+}
--- src/pmp/pmp_test.cpp
+++ src/pmp/pmp_test.cpp
@@ -1,1 +1,33 @@
 #include "pmp_test.h"
+
+void pmp_test::pmp_test(const std::string &input_file, const std::string &out_file, const double &angle) {
+    auto time0 = std::chrono::steady_clock::now();
+    pmp::SurfaceMesh mesh;
+    pmp::read(mesh, input_file);
+	
+    int nv = mesh.n_vertices();
+    pmp::decimate(mesh, nv * 0.3);
+
+     //pmp::decimate(mesh,
+     //             nv * 0.3, // target vertices
+     //        8.0,    // aspect ratio
+     //        0.0,
+     //        12,                    // max valence
+     //             angle, // normal deviation
+     //        0.0);
+
+
+    pmp::write(mesh, out_file);
+
+   
+
+        std::cout
+        << "decimated V: " << mesh.n_vertices() << " F: " << mesh.n_faces() << std::endl;
+
+    auto time1 = std::chrono::steady_clock::now();
+
+    double seconds =
+        (double)std::chrono::duration_cast<std::chrono::milliseconds>(time1 - time0).count() / 1000.0;
+
+    std::cout << "OpenMeshºÄÊ±£º " << seconds << " s." << std::endl;
+}
