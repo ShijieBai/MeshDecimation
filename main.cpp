7844e7172e8180a2fcded8e87f790470ce57361b
baishijie <1139179328@qq.com>
2026-03-12 15:05:04 +0800

    save

--- main.cpp
+++ main.cpp
@@ -5,18 +5,19 @@
 
 int main() {
 
-    std::string input_file = R"(D:\A-work\mesh\MeshDecimation\MeshDecimation\input\2.stl)";
-    std::string out_file = R"(D:\A-work\mesh\MeshDecimation\MeshDecimation\output\2.stl)";
+    std::string input_file = R"(D:\A-work\mesh\MeshDecimation\MeshDecimation\input\tpms-1.stl)";
+    std::string out_file = R"(D:\A-work\mesh\MeshDecimation\MeshDecimation\output\3.stl)";
 
     double sharp_angle_deg = 45.0;
     double sharp_angle_rad = sharp_angle_deg * M_PI / 180.0;   // math defined
 
+    double target_percent{0.5};
   
     // CGAL mesh decimation
      //cgal::CGAL_test(input_file, out_file, sharp_angle_rad);
 
     // OpenMesh mesh decimation
-     OpenMesh::OpenMesh_test(input_file, out_file, sharp_angle_rad);
+    OpenMesh::OpenMesh_test(input_file, out_file, target_percent);
 
      // pmp mesh decimation
      //pmp_test::pmp_test(input_file, out_file, sharp_angle_rad);
